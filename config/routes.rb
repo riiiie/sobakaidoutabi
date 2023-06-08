af83cb2d8a9c1a36663f12a0bf46f9e544674433
riiiie <rieohya008@gmail.com>
2023-06-08 23:09:17 +0900

    Shop一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'shops/index'
-  get 'shops/new'
-  get 'shops/create'
-  get 'shops/show'
-  get 'shops/edit'
-  get 'shops/update'
-  get 'shops/destroy'
-  get 'user_sessions/new'
-  get 'user_sessions/create'
   root 'static_pages#top'
 
   get 'login', to: 'user_sessions#new'
@@ -15,4 +6,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resources :shops
 end
