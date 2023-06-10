4073259d68172a3c6bfe9a5b192fc4d6ad86aa28
riiiie <rieohya008@gmail.com>
2023-06-11 00:35:56 +0900

    Recordモデルにバリデーション追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,8 @@
 class Record < ApplicationRecord
+  validates :soba_texture, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :soba_smell, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :soba_aldente, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :soup_salt, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :soup_taste, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+
 end
